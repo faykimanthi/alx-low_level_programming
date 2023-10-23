64f9415febb366426376f935246a247947ff92a6
Faith <faykimanthi@gmail.com>
2023-10-23 12:27:54 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,11 +1,32 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * set_string - sets the value of a pointer to a char
- * @s: pointer to pointer we need to set to
- * @to: string to set
- */
-void set_string(char **s, char *to)
+  * print_diagsums - main function
+  *
+  * @size: function parameter
+  *
+  * @a: function parameter
+  *
+  * Return: Always 0.
+  */
+
+void print_diagsums(int *a, int size)
 {
-	*s = to;
+	int count1;
+	int count2;
+	int x;
+
+	count1 = 0;
+	count2 = 0;
+
+	for (x = 0; x < size; x++)
+	{
+	count1 = count1 + a[x * size + x];
+	}
+	for (x = size - 1; x >= 0; x--)
+	{
+	count2 += a[x * size + (size - x - 1)];
+	}
+	printf("%d, %d\n", count1, count2);
 }
