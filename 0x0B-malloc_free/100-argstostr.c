b487e3c0874f08b3b3692dbca5186b4d2188d93f
Faith <faykimanthi@gmail.com>
2023-10-30 15:25:25 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,15 +1,14 @@
 #include "main.h"
-		
 #include <stdlib.h>
-		
+
 /**
- * argstostr - main entry		
+ * argstostr - main entry
  * @ac: int input
  * @av: double pointer array
  * Return: 0
  */
 
-char *argstostr(int ac, char **av)		
+char *argstostr(int ac, char **av)
 {
 
 	int i, n, r = 0, l = 0;
@@ -33,7 +32,7 @@ char *argstostr(int ac, char **av)
 
 	for (i = 0; i < ac; i++)
 	{
-	for (n = 0; av[i][n]; n++)	
+	for (n = 0; av[i][n]; n++)
 	{
 		str[r] = av[i][n];
 		r++;
